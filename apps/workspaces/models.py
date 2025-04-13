81bd41d9ab32d1cd2d9b01521351a5409c3e9c57
Randy <anakkantorku@gmail.com>
2025-04-13 17:20:24 +0700

    Create class Departement & DepartementMember

--- apps/workspaces/models.py
+++ apps/workspaces/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from core.models import BaseModel
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Departement(BaseModel):
+    name = models.CharField(max_length=100)
+
+class DepartementMember(BaseModel):
+    departemnet = models.ForeignKey(Departement, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
