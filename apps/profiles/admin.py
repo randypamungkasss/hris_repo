68a7cbd3d1d666e77e8acce4f0f286b61fcd4762
Randy <anakkantorku@gmail.com>
2025-04-13 17:50:52 +0700

    Register models Profile to Admin and Setup list_display

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio', 'address', 'created_at', 'updated_at')
+
+admin.site.register(Profile, ProfileAdmin)
