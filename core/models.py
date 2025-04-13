4d202f05201e76a29913e7cfba838d1a12de720a
Randy <anakkantorku@gmail.com>
2025-04-13 16:59:23 +0700

    Install black & isort and Running

--- core/models.py
+++ core/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+
 from .utils import generate_id
 
+
 class BaseModel(models.Model):
-    id = models.CharField(primary_key=True, default=generate_id, editable=False, max_length=100)
+    id = models.CharField(
+        primary_key=True, default=generate_id, editable=False, max_length=100
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
