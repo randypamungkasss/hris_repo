a3ece1519d80b5f4e60c8d821387f352d08e8997
Randy <anakkantorku@gmail.com>
2025-04-13 17:37:08 +0700

    Create fields is_active and Setup Filter, and Setup Search Bar for Class DepartementAdmin

--- apps/workspaces/admin.py
+++ apps/workspaces/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Departement, DepartementMember
 
 # Register your models here.
+class DepartementAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at', 'is_active')
+    list_filter = ('created_at', 'updated_at', 'is_active')
+    search_fields = ('name',)
+
+admin.site.register(Departement, DepartementAdmin)
--- apps/workspaces/models.py
+++ apps/workspaces/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Departement(BaseModel):
     name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
 
 class DepartementMember(BaseModel):
     departemnet = models.ForeignKey(Departement, on_delete=models.CASCADE)
