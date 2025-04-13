86634baa6259c8e109f2138fd0ce5580ea23d263
Randy <anakkantorku@gmail.com>
2025-04-13 17:15:44 +0700

    Create class Profile in /apps/profiles/models.py

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from core.models import BaseModel
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
